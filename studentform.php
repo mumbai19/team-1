31ecca4a1a5936e4c3fa51f71e731ef23ead65f2
maitri <maitrithaker2@gmail.com>
2019-07-20 21:13:05 +0530

    database

--- studentform.php
+++ studentform.php
@@ -2,41 +2,22 @@
 	//require "config.php";
 	
 	require('config.php');
-	
+	$conn=mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_DATABASE);
 	session_start();
+	$name=$_GET['name'];
+	$phno=	$_GET['phno'];
+	$address=$_GET['address'];
+	$school=$_GET['school'];
+	$std=$_GET['std'];
+
+
 	
-	$_SESSION['invalidCredentialsMessage'] = "Username ot password incorrect!";
 	
-	$username = $_POST["username"];
-	$password = $_POST["password"];
 	
-	//$username = "172010001";
-	//$password = "Kailas123";
 	
-	$mysql_query = "SELECT username, password, usertype FROM users WHERE username='$username' AND password='$password';";
+	$mysql_query = "UPDATE studentdetails SET name = $name, phno = $phno ,address=$address,school=$school,std=$std " ;
 	
-	$result = mysqli_query($con, $mysql_query);
+	$mysqli_query($conn, $mysql_query);
 	
-	if(mysqli_num_rows($result)>0){
-		$row = mysqli_fetch_assoc($result);
-		$fetchedUsername = $row["username"];
-		//$fetchedPassword = $row["password"];
-		$fetchedUserType = $row["usertype"];
-		$_SESSION['username'] = $fetchedUsername;
-		if($fetchedUserType == 'S'){
-			
-			//header('location: studentMainScreen_Attendance.php');
-			header('location: gallery.html');
-			
-		}else{
-			header('location: gallery.html');
-			$_SESSION['username'] = $fetchedUsername;
-		}
-		//echo "Login Successful";
-		unset($_SESSION['invalidCredentialsMessage']);
-	}
-	else{
-		//echo "Login unsuccessful :(";
-		header('location: index.php?invalidCredentials=1');
-	}
+	
 ?>
